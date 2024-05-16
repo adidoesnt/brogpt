4a5ab9d94bbf43c8c08a1306efb221288061803f
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-05-16 17:20:56 +0800

    add local dynamodb initialisation

--- backend/user/src/components/database.rs
+++ backend/user/src/components/database.rs
@@ -1,5 +1,5 @@
-use aws_sdk_dynamodb as dynamodb;
-use aws_config::{load_defaults, BehaviorVersion};
+use aws_sdk_dynamodb::Client;
+use aws_config::BehaviorVersion;
 
 pub struct Database {
     pub client: aws_sdk_dynamodb::Client,
@@ -7,8 +7,14 @@ pub struct Database {
 
 impl Database {
     pub async fn new() -> Self {
-        let config = load_defaults(BehaviorVersion::v2024_03_28()).await;
-        let client = dynamodb::Client::new(&config);
+        let config = aws_config::defaults(BehaviorVersion::latest())
+            .test_credentials()
+            .load()
+            .await;
+        let dynamodb_local_config = aws_sdk_dynamodb::config::Builder::from(&config)
+            .endpoint_url("http://localhost:8000")
+            .build();
+        let client = Client::from_conf(dynamodb_local_config);
         Database { client }
     }
 }
