bd46cddb6b8c256aabb34289ddd012924cb27396
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-05-16 11:39:12 +0800

    add health check for user service

--- backend/user/src/main.rs
+++ backend/user/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use std::env;
+use dotenv::dotenv;
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Server is healthy")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
+    let hostname = format!("{}:{}", host, port);
+
+    println!("Starting server at {}", hostname);
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind(hostname)?
+    .run()
+    .await
 }
