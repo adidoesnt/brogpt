0a885f36280dbc58302e1de6c0d4f19c4b0fab49
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-05-17 10:47:46 +0800

    add types to database component

--- backend/user/src/components/database.rs
+++ backend/user/src/components/database.rs
@@ -12,18 +12,18 @@ impl Database {
     pub async fn new() -> Self {
         dotenv().ok();
 
-        let host = env::var("DB_HOST").unwrap_or_else(|_| "localhost".to_string());
-        let port = env::var("DB_PORT").unwrap_or_else(|_| "8000".to_string());
-        let hostname = format!("{}:{}", host, port);
+        let host: String = env::var("DB_HOST").unwrap_or_else(|_| "localhost".to_string());
+        let port: String = env::var("DB_PORT").unwrap_or_else(|_| "8000".to_string());
+        let hostname: String = format!("{}:{}", host, port);
 
-        let config = aws_config::defaults(BehaviorVersion::latest())
+        let config: aws_config::SdkConfig = aws_config::defaults(BehaviorVersion::latest())
             .test_credentials()
             .load()
             .await;
-        let dynamodb_local_config = aws_sdk_dynamodb::config::Builder::from(&config)
+        let dynamodb_local_config: aws_sdk_dynamodb::Config = aws_sdk_dynamodb::config::Builder::from(&config)
             .endpoint_url(hostname)
             .build();
-        let client = Client::from_conf(dynamodb_local_config);
+        let client: Client = Client::from_conf(dynamodb_local_config);
         Database { client }
     }
 }
